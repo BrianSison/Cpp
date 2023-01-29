fb763a349d99712a23e3954d88755669c46db579
Brian Sison <brian.nosis12@gmail.com>
2023-01-29 11:13:42 +0800

    Update QUEUE.cpp

--- Data Structures/Queue/QUEUE.cpp
+++ Data Structures/Queue/QUEUE.cpp
@@ -1,183 +1,171 @@
 #include<iostream>
 
-class QUEUE{
+class Queue{
     private:
-        int queue[5], index=-1, temp_element, queue_element, front_element, pop_element, insert_element, customer;
+        int frontElement, backElement, popElement, peekElement, tempElement, insertElement, queueElemet, size = 1;
+        int* queue=new int[size];
     public:
-        void clear(){
-            for(int x=0;x<5;x++){
-                queue[x]=0;
-            }
-            index=-1;
+        void clearQueue(){
+            delete[] queue;
+            size=1;
         }
-        int size(){
-            return index;
+        void queueSize(){
+            if(size==1){
+                std::cout << "\nQueue is empty...\n";
+            }
+            else{
+                std::cout << "\nQueue size is " << size-1<< std::endl;
+            }
         }
         void inqueue(int x){
-            if(size()!=4){
-                index++;
-                queue[index]=x;
-            }else{
-                std::cout << "\nError, Queue is full...";
-            }
-        }
-        void push(int x, int y){
-            if(x<=5){
-                if(queue[x-1]==0){
-                    queue[x-1]=y;
-                }else{
-                    std::cout << " [Error, slot not available]";
-                }
-            }else{
-                std::cout << " [Error, slot out of reach]";
-            }
-        }
-        int insert(){
-            return insert_element;
+            queue[size-1]=x;
+            queue[size]=0;
+            size++;
         }
         void dequeue(){
-            front_element=queue[0];
+            frontElement=queue[0];
+            size--;
             queue[0]=0;
-            index--;
             shift();
         }
-        void status(){
-            if(size()==4) std::cout << " [Queue is full]";
-            else if(size()==-1) std::cout << " [Queue is empty]";
-            else std::cout << " [Queue size is " << size()+1 << "]";
+        void shift(){
+            int x=0;
+            do{
+                if(queue[x]==0){
+                    tempElement=queue[x+1];
+                    queue[x]=tempElement;
+                    queue[x+1]=0;
+                }
+                x++;
+            }while(x<size);
+        }
+        void display(){
+            std::cout << "\nQueue: ";
+            for(int x=0;x<size;x++){
+                if(queue[x]==0) continue;
+                else std::cout << queue[x] << " ";
+            }
+            std::cout << std::endl;
+        }
+        int frontQueue(){
+            frontElement=queue[0];
+            return frontElement;
+        }
+        int backQueue(){
+            if(size!=1){
+                backElement=queue[size-2];
+            }
+            else{
+                backElement=queue[size-1];
+            }
+            return backElement;
+        }
+        void remove(int x){
+            size--;
+            popElement = queue[x-1];
+            queue[x-1]=0;
+            shift();
+        }
+        int popQueue(){
+            return popElement;
         }
         void swap(int x, int y){
-            if((x!=0 && x<=4) && (y!=0 && y<=4)){
-                temp_element=queue[x-1];
+            if((x!=0 && x<size) && (y!=0 && y<size)){
+                tempElement=queue[x-1];
                 queue[x-1]=queue[y-1];
-                queue[y-1]=temp_element;
-            }else{
-                std::cout << " [Error, invalid input]\n";
+                queue[y-1]=tempElement;
+            }
+            else{
+                std::cout << "Error, invalid input\n";
             }
         }
         void change(int x, int y){
-            if(x!=0 && x<=5){
+            if((x!=0 && x<size) && y!=0){
                 queue[x-1]=y;
-            }else{
-                std::cout << " [Error, element out of reach]\n";
+            }
+            else{
+                std::cout << "Error, invalid input\n";
             }
         }
-        void remove(int x){
-            if(x>0 && x<=5){
-                if(queue[x-1]!=0){
-                    pop_element=queue[x-1];
-                    queue[x-1]=0;
-                    index--;
-                }else{
-                    std::cout << " [Error, no element value]";
-                }
-            }else std::cout << " [Error, element out of reach]";
-        }
-        void shift(){
-            for(int x=0;x<5;x++){
-                if(queue[x]==0 && queue[x+1]!=0){
-                    if(x!=4){
-                        queue_element=queue[x+1];
-                        queue[x]=queue[x+1];
-                        queue[x+1]=0;
-                    }
-                    else{
-                        queue[x]=0;
-                    }
-                }
+        void peekQueue(int x){
+            if(x!=0 && x<size){
+                peekElement = queue[x-1];
+            }
+            else{
+                peekElement=0;
+                std::cout << "Error, invalid input\n";
             }
         }
-        void display(){
-            std::cout << "Queue: ";
-            for(int x=0;x<5;x++){
-                std::cout << queue[x];
-            }
+        int peek(){
+            return peekElement;
         }
-        int pop(){
-            return pop_element;
-        }
-        int front(){
-            return front_element;
-        }
-        int back(){
-            return queue[size()];
-        }
-};
+};  
 
 int main(){
-    QUEUE DS;
+    Queue DS;
 
-    system("cls");
+    std::cout << "\"Generating Queue\"\n";
+    DS.clearQueue();
+    DS.queueSize();
 
-    std::cout << "\nGenerates empty Queue... ";
-    DS.clear();
-    DS.display();
-    
-    std::cout << "\nChecks Queue size... ";
-    DS.status();
-    
-    std::cout << "\n\nAdding elements... ";
-    for(int x=0;x<5;x++){
+    std::cout << "\"Inqueue\"\n";
+    for(int x=0;x<10;x++){
         DS.inqueue(x+1);
     }
     DS.display();
-    
-    std::cout << "\nChecks Queue size... ";
-    DS.status();
-    
-    std::cout << "\n\nOverloading the Queue by adding new element (7)... ";
-    DS.inqueue(7);
-    
-    std::cout << "\n\nRemoving the first element... ";
+    DS.queueSize();
+    std::cout << "front: " << DS.frontQueue() << " back: " << DS.backQueue() << std::endl;
+
+    std::cout << "\n\"Dequeue\"";
+    //1
     DS.dequeue();
     DS.display();
+    std::cout << "front: " << DS.frontQueue() << " back: " << DS.backQueue();
 
-    std::cout << "\nChecks Queue size...";
-    DS.status();
-
-    std::cout << "\n\nDislay first and last element... [front: " << DS.front() << " back: " << DS.back() << "]";
-
-    std::cout << "\n\nRemoves the specific element... Removing the (7) element...";
-    DS.remove(7);
-
-    std::cout << "\nRemoves the specific element... Removing the (5) element...";
-    DS.remove(5);
-    
-    std::cout << "\nRemoves the specific element... Removing the (1) element...";
-    DS.remove(1);
-    
-    std::cout << " Display the pop element: " << DS.pop() << std::endl;
+    //2
+    DS.dequeue();
     DS.display();
-    
-    std::cout << "\nChecks Queue size...";
-    DS.status();
+    std::cout << "front: " << DS.frontQueue() << " back: " << DS.backQueue();
 
-    std::cout << "\n\nSwaping the elements... ";
-    std::cout << "\nSwapping 1 and 2 element... swap(1,2) ";
+    //3
+    DS.dequeue();
+    DS.display();
+    std::cout << "front: " << DS.frontQueue() << " back: " << DS.backQueue();
+
+    //4
+    DS.dequeue();
+    DS.display();
+    std::cout << "front: " << DS.frontQueue() << " back: " << DS.backQueue();
+
+    //5
+    std::cout << "\n\n\"Remove\"";
+    DS.remove(6);
+    DS.display();
+    std::cout << "front: " << DS.frontQueue() << " back: " << DS.backQueue() << std::endl;
+    std::cout << "Pop: " << DS.popQueue() << std::endl;
+
+    //6
+    std::cout << "\n\n\"Swap\"";
     DS.swap(1,2);
     DS.display();
-    
-    std::cout << "\nSwapping 1 and 6 element... swap(1,6) ";
-    DS.swap(1,6);
+    DS.swap(1,10);
     DS.display();
+    std::cout << "front: " << DS.frontQueue() << " back: " << DS.backQueue() << std::endl;
 
-    std::cout << "\n\nChanging the element... change(1 to 8) ";
-    DS.change(1,8);
+    //7
+    std::cout << "\n\n\"Change\"";
+    DS.change(5,110);
     DS.display();
-    
-    std::cout << "\nChanging the element... change(6 to 8) ";
-    DS.change(6,8);
+    DS.change(6,100);
     DS.display();
+    std::cout << "front: " << DS.frontQueue() << " back: " << DS.backQueue() << std::endl;
 
-    std::cout << "\n\nInserting element... inserting(7 on index 4)... ";
-    DS.push(4,7);
-    
-    std::cout << "\nInserting element... inserting(5 on index 6)... ";
-    DS.push(6,5);
-    
-    std::cout << "\nInserting element... inserting(3 on index 2)... ";
-    DS.push(2,3);
-    DS.display();
+    //8
+    std::cout << "\n\"Peek\"";
+    DS.peekQueue(1);
+    std::cout << "\nPeek: " << DS.peek() << std::endl;
+    DS.peekQueue(100);
+    std::cout << "Peek: " << DS.peek() << std::endl;
 
-    return 0;
+    DS.clearQueue();
 }
